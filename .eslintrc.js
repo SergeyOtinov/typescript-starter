12e06da5a5635b965a96c4ecf5b9a1de4abc901c
Sergey <otinov.prog@gmail.com>
2024-06-05 17:03:45 +0300

    CRM 1.03

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   root: true,
   env: {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from './Users/users.module'
+import { User } from './graphql/models/User'
+import { Job } from './graphql/models/Job'
+import { AuthModule } from './Auth/auth.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+   imports: [
+      GraphQLModule.forRoot<ApolloDriverConfig>({
+         driver: ApolloDriver,
+         autoSchemaFile: 'src/schema.gql'
+      }),
+      ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env`, isGlobal: true }),
+      TypeOrmModule.forRoot({
+         type: 'postgres',
+         host: 'localhost',
+         port: Number(process.env.DATABASE_PORT),
+         username: 'postgres',
+         password: process.env.DATABASE_PASSWORD,
+         database: process.env.DATABASE_NAME,
+         entities: [User, Job],
+         synchronize: true
+      }),
+      UsersModule,
+      AuthModule
+   ]
 })
 export class AppModule {}
